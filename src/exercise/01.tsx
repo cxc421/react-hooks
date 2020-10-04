a2b4f701c2b9c6a4bb9887d8b86b5050cd8601b5
Chris Chuang <chrischuang0421@gmail.com>
2020-10-04 21:30:40 +0800

    Complete exercise 1 - useState

--- src/exercise/01.tsx
+++ src/exercise/01.tsx
@@ -1,29 +1,32 @@
 // useState: greeting
 // http://localhost:3000/isolated/exercise/01.js
 
-import React from 'react';
+import React, {FC, useState} from 'react';
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = '';
+type GreetingProps = {
+  initialName?: string;
+};
+
+const Greeting: FC<GreetingProps> = ({initialName = ''}) => {
+  const [name, setName] = useState(initialName);
 
   function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value);
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
   );
-}
+};
 
 function App() {
-  return <Greeting />;
+  return <Greeting initialName="Chris" />;
 }
 
 export default App;
