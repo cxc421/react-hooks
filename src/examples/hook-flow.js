10c427f2e1abdd546ebd4d35a257612dabf9f202
Chris Chuang <chrischuang0421@gmail.com>
2020-10-05 11:19:58 +0800

    Add comments for hooks-flow

--- src/examples/hook-flow.js
+++ src/examples/hook-flow.js
@@ -2,94 +2,100 @@
 // https://github.com/donavon/hook-flow
 // http://localhost:3000/isolated/examples/hook-flow.js
 
-import React from 'react'
+// 1. 注意, Parent render 完後, Child 才開始 render
+// 2. 注意, 同一Component 中 所有上一輪的 effect 都 chelan up 後, 才會依序執行新一輪的 effect
+// ex: [Mount] Parent Init => Parent Render => Child Init => Child Render => Child Effect => Parent Effect
+// ex: [Update] Parent Render => Child Render => Child Clean Effect => Child Effect => Parent Clean Effect => Parent Effect
+// ex: [Unmount] Parent Render => Child Clen Effect => Parent Clean Effect => Parent Effect
+
+import React from 'react';
 
 function Child() {
-  console.log('%c    Child: render start', 'color: MediumSpringGreen')
+  console.log('%c    Child: render start', 'color: MediumSpringGreen');
 
   const [count, setCount] = React.useState(() => {
-    console.log('%c    Child: useState(() => 0)', 'color: tomato')
-    return 0
-  })
+    console.log('%c    Child: useState(() => 0)', 'color: tomato');
+    return 0;
+  });
 
   React.useEffect(() => {
-    console.log('%c    Child: useEffect(() => {})', 'color: LightCoral')
+    console.log('%c    Child: useEffect(() => {})', 'color: LightCoral');
     return () => {
       console.log(
         '%c    Child: useEffect(() => {}) cleanup 🧹',
         'color: LightCoral',
-      )
-    }
-  })
+      );
+    };
+  });
 
   React.useEffect(() => {
     console.log(
       '%c    Child: useEffect(() => {}, [])',
       'color: MediumTurquoise',
-    )
+    );
     return () => {
       console.log(
         '%c    Child: useEffect(() => {}, []) cleanup 🧹',
         'color: MediumTurquoise',
-      )
-    }
-  }, [])
+      );
+    };
+  }, []);
 
   React.useEffect(() => {
-    console.log('%c    Child: useEffect(() => {}, [count])', 'color: HotPink')
+    console.log('%c    Child: useEffect(() => {}, [count])', 'color: HotPink');
     return () => {
       console.log(
         '%c    Child: useEffect(() => {}, [count]) cleanup 🧹',
         'color: HotPink',
-      )
-    }
-  }, [count])
+      );
+    };
+  }, [count]);
 
   const element = (
     <button onClick={() => setCount(previousCount => previousCount + 1)}>
       {count}
     </button>
-  )
+  );
 
-  console.log('%c    Child: render end', 'color: MediumSpringGreen')
+  console.log('%c    Child: render end', 'color: MediumSpringGreen');
 
-  return element
+  return element;
 }
 
 function App() {
-  console.log('%cApp: render start', 'color: MediumSpringGreen')
+  console.log('%cApp: render start', 'color: MediumSpringGreen');
 
   const [showChild, setShowChild] = React.useState(() => {
-    console.log('%cApp: useState(() => false)', 'color: tomato')
-    return false
-  })
+    console.log('%cApp: useState(() => false)', 'color: tomato');
+    return false;
+  });
 
   React.useEffect(() => {
-    console.log('%cApp: useEffect(() => {})', 'color: LightCoral')
+    console.log('%cApp: useEffect(() => {})', 'color: LightCoral');
     return () => {
-      console.log('%cApp: useEffect(() => {}) cleanup 🧹', 'color: LightCoral')
-    }
-  })
+      console.log('%cApp: useEffect(() => {}) cleanup 🧹', 'color: LightCoral');
+    };
+  });
 
   React.useEffect(() => {
-    console.log('%cApp: useEffect(() => {}, [])', 'color: MediumTurquoise')
+    console.log('%cApp: useEffect(() => {}, [])', 'color: MediumTurquoise');
     return () => {
       console.log(
         '%cApp: useEffect(() => {}, []) cleanup 🧹',
         'color: MediumTurquoise',
-      )
-    }
-  }, [])
+      );
+    };
+  }, []);
 
   React.useEffect(() => {
-    console.log('%cApp: useEffect(() => {}, [showChild])', 'color: HotPink')
+    console.log('%cApp: useEffect(() => {}, [showChild])', 'color: HotPink');
     return () => {
       console.log(
         '%cApp: useEffect(() => {}, [showChild]) cleanup 🧹',
         'color: HotPink',
-      )
-    }
-  }, [showChild])
+      );
+    };
+  }, [showChild]);
 
   const element = (
     <>
@@ -113,11 +119,11 @@ function App() {
         {showChild ? <Child /> : null}
       </div>
     </>
-  )
+  );
 
-  console.log('%cApp: render end', 'color: MediumSpringGreen')
+  console.log('%cApp: render end', 'color: MediumSpringGreen');
 
-  return element
+  return element;
 }
 
-export default App
+export default App;
