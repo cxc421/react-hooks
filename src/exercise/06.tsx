64bd9862c74f1fa3ad0f0bba1a5508ec4d498732
Chris Chuang <chrischuang0421@gmail.com>
2020-10-08 13:19:54 +0800

    Complete exercise 6-2~6-5: Raw Error Boundary

--- src/exercise/06.tsx
+++ src/exercise/06.tsx
@@ -15,25 +15,71 @@ type PokemonInfoProps = {
   pokemonName: string;
 };
 
+type PokemonState =
+  | {status: 'idle'}
+  | {status: 'pending'}
+  | {status: 'resolved'; pokemon: Pokemon}
+  | {status: 'rejected'; error: Error};
+
 const PokemonInfo: FC<PokemonInfoProps> = ({pokemonName}) => {
-  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+  const [pokemonState, setPokemonState] = useState<PokemonState>({
+    status: 'idle',
+  });
 
   useEffect(() => {
     if (!pokemonName) return;
-    setPokemon(null);
+    setPokemonState({status: 'pending'});
     fetchPokemon(pokemonName)
-      .then(pokemonData => setPokemon(pokemonData))
-      .catch(err => alert(err.message));
+      .then(pokemon => setPokemonState({status: 'resolved', pokemon}))
+      .catch(error => setPokemonState({status: 'rejected', error}));
   }, [pokemonName]);
 
-  if (!pokemonName) {
+  if (pokemonState.status === 'idle') {
     return <>Submit a pokemon</>;
   }
-  if (!pokemon) {
+
+  if (pokemonState.status === 'pending') {
     return <PokemonInfoFallback name={pokemonName} />;
   }
-  return <PokemonDataView pokemon={pokemon} />;
+
+  if (pokemonState.status === 'rejected') {
+    throw pokemonState.error;
+  }
+
+  return <PokemonDataView pokemon={pokemonState.pokemon} />;
 };
+
+type ErrorBoundaryProps = {FallbackComponent: FC<{error: Error}>};
+type ErrorBoundaryState = {error: Error | null};
+
+class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  state: ErrorBoundaryState = {error: null};
+  static getDerivedStateFromError(error: Error) {
+    // 更新 state 以至於下一個 render 會顯示 fallback UI
+    return {error};
+  }
+  // componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+  //   console.log(errorInfo);
+  //   this.setState({error});
+  // }
+  render() {
+    const {error} = this.state;
+    if (error) {
+      return <this.props.FallbackComponent error={error} />;
+    }
+    return this.props.children;
+  }
+}
+
+const ErrorFallback: FC<{error: Error}> = ({error}) => (
+  <div role="alert">
+    There was an error:{' '}
+    <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+  </div>
+);
 
 function App() {
   const [pokemonName, setPokemonName] = React.useState('');
@@ -47,7 +93,10 @@ function App() {
       <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
       <hr />
       <div className="pokemon-info">
-        <PokemonInfo pokemonName={pokemonName} />
+        {/* 利用 key 不同時會 unmount/mount 的特性, 可以 reset ErrorBoundary 的 state */}
+        <ErrorBoundary FallbackComponent={ErrorFallback} key={pokemonName}>
+          <PokemonInfo pokemonName={pokemonName} />
+        </ErrorBoundary>
       </div>
     </div>
   );
